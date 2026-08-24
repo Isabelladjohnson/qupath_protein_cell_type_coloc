9d3e9ccd96b33ed8528b908f5d0a75aca8565161
Isabella Johnson <isabelladjohnson@arizona.edu>
2026-08-24 16:35:08 -0700

    Replace channel name '63 (C2)' with 'protein (C2)' and update print message

--- 2_adjust_channel_min_and_max.groovy
+++ 2_adjust_channel_min_and_max.groovy
@@ -3,9 +3,9 @@ def display = viewer.getImageDisplay()
 
 for (def channel : display.availableChannels()) {
     def name = channel.getName()
-    if (name == '63 (C2)') {
+    if (name == 'protein (C2)') {
         display.setMinMaxDisplay(channel, 0, 2000)
-        print('63: min=0, max=2000')
+        print('protein: min=0, max=2000')
     } else if (name == 'iba1 (C3)') {
         display.setMinMaxDisplay(channel, 0, 1200)
         print('iba1: min=0, max=1200')
